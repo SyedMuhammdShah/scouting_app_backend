b4f1a289ceb613fb7424add048b90dc270d16ce9
AidenPearce125 <184442418+AidenPearce125@users.noreply.github.com>
2026-01-27 19:44:55 +0500

    do some changes

--- src/server.js
+++ src/server.js
@@ -3,9 +3,15 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 
+if (!process.env.MONGO_URI) {
+  console.error("FATAL ERROR: MONGO_URI is not defined in .env file.");
+  process.exit(1);
+}
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("MongoDB connected");
-  app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
+  const port = process.env.PORT || 5000;
+  app.listen(port, () =>
+    console.log(`Server running on port ${port}`)
   );
 });
